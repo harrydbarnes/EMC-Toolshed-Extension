373f238914011b9540bbda3f1c0e568c5f6823eb
Harry Barnes <145344818+harrydbarnes@users.noreply.github.com>
2024-08-22 17:01:32 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,14 @@
 document.getElementById('generateUrl').addEventListener('click', function() {
     const campaignId = document.getElementById('campaignId').value;
-    const campaignDate = document.getElementById('campaignDate').value;
+    let campaignDate = document.getElementById('campaignDate').value;
 
     if (campaignId && campaignDate) {
+        // Convert month name to "YYYY-MM-DD" format
+        const date = new Date(`${campaignDate} 1, ${new Date().getFullYear()}`);
+        const formattedDate = date.toISOString().split('T')[0]; // Get the date in YYYY-MM-DD format
+
         const baseUrl = 'https://groupmuk-prisma.mediaocean.com/campaign-management/#osAppId=prsm-cm-spa&osPspId=prsm-cm-buy&campaign-id=';
-        const finalUrl = `${baseUrl}${campaignId}&route=actualize&mos=${campaignDate}`;
+        const finalUrl = `${baseUrl}${campaignId}&route=actualize&mos=${formattedDate}`;
         
         chrome.tabs.create({ url: finalUrl });
     } else {

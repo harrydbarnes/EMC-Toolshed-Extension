c8d44fa054e1051c2d880580356ec0579aba3ff3
Harry Barnes <145344818+harrydbarnes@users.noreply.github.com>
2025-05-07 16:28:35 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -91,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function() {
     addClickListener('metaHandbookButton', 'https://insidemedia.sharepoint.com/sites/GRM-UK-GMS/Files%20Library/Forms/AllItems.aspx?id=%2Fsites%2FGRM%2DUK%2DGMS%2FFiles%20Library%2FChannel%5FSocial%2FPaid%20Social%20Prisma%20Integration%20Resources%2FLatest%20Handbook&p=true&ga=1'); // Added listener for Meta Handbook button
     addClickListener('timesheetsButton', 'https://groupmuk-aura.mediaocean.com/viewport-home/#osAppId=rod-time&osPspId=rod-time&route=time/display/myTimesheets/ToDo');
     addClickListener('approvalsButton', 'https://groupmuk-aura.mediaocean.com/viewport-home/#osAppId=rod-time&osPspId=rod-time&route=time/display/myTimesheetApprovals/AwaitingMe');
-    addClickListener('expensesButton', 'https://groupmuk-aura.mediaocean.com/viewport-home/#osAppId=rod-exps&osPspId=rod-exps&route=expenses/myApprovals/AwaitingMe');
+    // Removed addClickListener for Expenses button
     addClickListener('officeHoursButton', 'https://harrydbarnes.github.io/EssenceMediacomTools/');
     addClickListener('addCampaignButton', 'https://groupmuk-prisma.mediaocean.com/campaign-management/#osAppId=prsm-cm-spa&osPspId=cm-dashboard&route=campaigns&osModalId=prsm-cm-cmpadd&osMOpts=lb');
 
